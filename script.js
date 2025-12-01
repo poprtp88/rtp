b14b2f3a62107d7dc9977bb518104ffd2459e75b
poprtp88 <danila.gvargas@gmail.com>
2025-12-01 10:43:39 +0800

    ADD POPN1 AND POPDEZ

--- script.js
+++ script.js
@@ -124,7 +124,7 @@ const CONFIG = {
     platforms: [
         { id: 1, url: 'https://popduqo.com?ch=23890' },
         { id: 2, url: 'https://popx5t.com?ch=13250' },
-        { id: 3, url: 'BELOM ADA' }, // POPN1
+        { id: 3, url: 'https://popuptefa.com?ch=33323' }, // POPN1
         { id: 4, url: 'https://popbra.com/#/register?r_code=255862939718' },
         { id: 5, url: 'https://pop555.com/#/register?r_code=27363421531' },
         { id: 6, url: 'https://www.popbem66.com/#/register?r_code=62548100237' },
@@ -135,7 +135,7 @@ const CONFIG = {
         { id: 11, url: 'https://26bet.com/?id=911719620' },
         { id: 12, url: 'https://poppg.com/#/register?r_code=87311374506' },
         { id: 13, url: 'https://q5gdw6.com?ch=2291' },
-        { id: 14, url: 'BELOM ADA' }, // POPDEZ
+        { id: 14, url: 'https://popdezem.com?ch=30988' }, // POPDEZ
         { id: 15, url: 'https://9zqllv.com?ch=17356' },
         { id: 16, url: 'https://popceu.com/#/register?r_code=46223100109' },
         { id: 17, url: 'https://poplud.com?ch=30282' }, 
@@ -746,11 +746,6 @@ function generatePlatformCards() {
         
         card.addEventListener('click', function(e) {
             e.preventDefault();
-            
-            // Disable click for specific IDs
-            if (platform.id === 3 || platform.id === 14) {
-                return;
-            }
 
             const url = this.getAttribute('data-url');
             
